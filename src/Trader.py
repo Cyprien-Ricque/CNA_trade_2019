fe96d5af72b21a6f53c015241496c307838b97d7
Cyprien <cyprien.ricque@epitech.eu>
2020-05-12 12:17:28 +0200

    Make IA faster

--- src/Trader.py
+++ src/Trader.py
@@ -16,6 +16,8 @@ class Trader:
         self.wallet_ = Wallet(None)
         self.save_ = os.dup(1)
         self.trainTime_ = True
+        self.startIter = 50
+        self.currentIter = 0
 
     def run(self):
         self.parser_.getNextLine()
@@ -24,11 +26,15 @@ class Trader:
         if self.parser_.getDataType() == 'setting':
             if 'transaction_fee_percent' in self.parser_.setting().keys():
                 self.wallet_.setFee(self.parser_.setting()['transaction_fee_percent'])
+            if 'candles_given' in self.parser_.setting().keys():
+                self.startIter = int(self.parser_.setting()['candles_given'][0]) - 80
 
         if self.parser_.getDataType() == 'candle':
-            self.strategy_.newData(data['USDT_ETH'])
-            self.wallet_.updateLinks(data)
-            self.strategy_.calcIndicators()
+            self.currentIter += 1
+            if self.currentIter >= self.startIter:
+                self.strategy_.newData(data['USDT_ETH'])
+                self.wallet_.updateLinks(data)
+                self.strategy_.calcIndicators()
 
         if self.parser_.getDataType() == 'stack':
             self.wallet_.updateWallet(data)
@@ -37,9 +43,12 @@ class Trader:
             if self.trainTime_ is True:
                 self.strategy_.train()
                 self.trainTime_ = False
-            # os.dup2(self.save_, 1)
+            os.dup2(self.save_, 1)
             os.write(1, self.strategy_.predict(self.wallet_).encode())
-            # os.dup2(2, 1)
+            os.dup2(2, 1)
 
 
 # Engine out: 'bot 0 send update game next_candles BTC_ETH,1516147200,0.095,0.09181,0.09219501,0.09199999,481.51276914;USDT_ETH,1516147200,1090.1676815,1022.16791604,1023.1,1029.99999994,1389783.7868468;USDT_BTC,1516147200,11600.12523891,11032.9211865,11041.42197477,11214.06052489,4123273.6568455'
+
+
+
